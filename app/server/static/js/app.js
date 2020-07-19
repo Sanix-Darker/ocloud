5d31e2941043f845dbb428e0f611808af5bc642c
sanix-darker <s4nixd@gmail.com>
2020-07-19 02:30:38 +0100

    Adding a security on checking if file input is empty or not + some stuffs like that

--- app/server/static/js/app.js
+++ app/server/static/js/app.js
@@ -25,26 +25,34 @@ const send_file = () => {
     if (chatId === "" && chatId.length <= 5){
         alert("Provide a valid chat_id to save a file !")
     }else{
-        document.getElementById("response").innerHTML = "Sending the file..."
-        formData.append("chat_id", chatId);
-        // HTML file input, chosen by user
-        formData.append("file", fileElement.files[0]);
-        request.open("POST", "/api/upload");
-        request.onload  = function() {
-            console.log("[+] res: ", request.response);
-            const responseHtml = `
-                    <b>- File-name :</b> <i style="color: #0069ff;">${JSON.parse(request.response)["json_map"]["file"]["file_name"]}</i>
-                    <br/><b>- File-key :</b> <i style="color: red;">${JSON.parse(request.response)["file_key"]}</i>
-                    <br/><b>- Chunks :</b> ${ObjectLength(JSON.parse(request.response)["json_map"]["file_map"])}
-                    <br/><b>- Timestamp :</b> ${new Date().toUTCString()} <br/>`;
-            if (ObjectLength(JSON.parse(request.response)["json_map"]["file_map"]) <= 0){
-                document.getElementById("response").innerHTML = "<i style='color: red;'>Something went wrong, please check your chat-id</i>";
-            }else{
-                document.getElementById("response").innerHTML = responseHtml;
-            }
-            // do something with jsonResponse
-         };
-        request.send(formData);
+        if(fileElement.files.length == 0 ){
+            alert("Provide a file please !")
+        }else{
+            document.getElementById("response").innerHTML = "Sending the file..."
+            formData.append("chat_id", chatId);
+            // HTML file input, chosen by user
+            formData.append("file", fileElement.files[0]);
+            request.open("POST", "/api/upload");
+            request.onload  = function() {
+                console.log("[+] res: ", request.response);
+                const responseHtml = `
+                        <b>- File-name :</b> <i style="color: #0069ff;">${JSON.parse(request.response)["json_map"]["file"]["file_name"]}</i>
+                        <br/><b>- File-key :</b> <i style="color: red;">${JSON.parse(request.response)["file_key"]}</i>
+                        <br/><b>- Chunks :</b> ${ObjectLength(JSON.parse(request.response)["json_map"]["file_map"])}
+                        <br/><b>- Timestamp :</b> ${new Date().toUTCString()} <br/>`;
+
+                if(JSON.parse(request.response)["status"] === "error"){
+                    document.getElementById("response").innerHTML = "<i style='color: red;'>Something went wrong, please check your chat-id</i>";
+                }else{
+                    if (ObjectLength(JSON.parse(request.response)["json_map"]["file_map"]) <= 0){
+                        document.getElementById("response").innerHTML = "<i style='color: red;'>Something went wrong, please check your chat-id</i>";
+                    }else{
+                        document.getElementById("response").innerHTML = responseHtml;
+                    }
+                }
+             };
+            request.send(formData);
+        }
     }
 }
 
@@ -62,10 +70,14 @@ const get_file = () => {
                     <br/><b>- File-key :</b> <i style="color: red;">${JSON.parse(request.response)["file_key"]}</i>
                     <br/><b>- Download-link :</b> <a href='${JSON.parse(request.response)["download_link"]}' target='_blank'>Click here to download the file</a>
                     <br/><b>- Node :</b>'All files are deleted 24 hours after creating their download link.'`;
-            if (typeof JSON.parse(request.response)["download_link"] === "undefined"){
-                document.getElementById("response2").innerHTML = "<i style='color: red;'>Something went wrong, please check your file-key.</i>";
+            if(JSON.parse(request.response)["status"] === "error"){
+                document.getElementById("response2").innerHTML = "<i style='color: red;'>Something went wrong, please check your file-key/i>";
             }else{
-                document.getElementById("response2").innerHTML = responseHtml;
+                if (typeof JSON.parse(request.response)["download_link"] === "undefined"){
+                    document.getElementById("response2").innerHTML = "<i style='color: red;'>Something went wrong, please check your file-key.</i>";
+                }else{
+                    document.getElementById("response2").innerHTML = responseHtml;
+                }
             }
         }
         request.send(null);
