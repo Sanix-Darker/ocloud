67ec6abd2789de3e1c36c2f95c9e71b0b26fb23d
sanix-darker <s4nixd@gmail.com>
2020-07-20 14:14:34 +0100

    Adding Download link on modal + auto-generate it while browsing + performnces improvements

--- app/server/static/js/app.js
+++ app/server/static/js/app.js
@@ -1,4 +1,7 @@
 const request = new XMLHttpRequest();
+let savedModalContent = null;
+let datas = null;
+let chatId = null;
 
 const init = () => {
 	var router = new Router([
@@ -28,89 +31,100 @@ const copy = () => {
 	console.log("Copied the text: " + fileKey.value)
 }
 
+const showSavedModal = (data = null) => {
+    if (data !== null){
+        savedModalContent = new BSN.Modal("#hiw", {
+            content: `<div class="modal-body">
+            <p class="alert alert-success">
+            🎉 ${data.message}<p/>
+            <p>
+                <b>File name :</b> 
+                <span class="text-primary">
+                    ${data.json_map.file.file_name}
+                    </span>
+            </p>
+            <label for="fileKey"><b>File key : </b></label>
+            <div class="input-group input-group-sm mb-3">
+                <input type="text" class="form-control"
+                value="${data.file_key}" id="fileKey">
+                <div class="input-group-append">
+                <button type="button" class="btn btn-secondary" onclick="copy()">Copy</button>
+                </div>
+            </div>
+            <p>
+                <b>Download-link :</b> 
+                <a target="_blank" class="h6 small" href="${location.href + "api/file/" + data.file_key}">
+                    ${location.href + "api/file/" + data.file_key}
+                </a>
+            </p>
+            <section>
+                <b>Chunks :</b> ${data.json_map.cloud_map.length}
+            </section>
+            <section><b>Timestamp :</b> ${new Date().toUTCString()}</section>
+            <p class="alert alert-warning">Make sure to save that <b>File key</b>, because it's the only key that will allow you to regenerate your file !<p/>
+                    </div>`,
+            backdrop: true,
+            keyboard: true,
+        });
+    }
+    savedModalContent.show()
+}
+
 const uploadFile = (event) => {
 	event.preventDefault()
-	event.target.setAttribute("disabled", "true")
-	const datas = new FormData()
-	const chatId = document.getElementById("chat_id").value
-	const fileElement = document.getElementById("file_id")
+    
+    datas = new FormData()
+    chatId = document.getElementById("chat_id").value
+    const fileElement = document.getElementById("file_id")
 
-    localStorage.setItem("chatId", chatId);
+    if (chatId === "" && chatId.length <= 5) {
+        alert("Provide a valid chat_id to save a file !")
+    } else {
+        if (fileElement.files.length === 0) {
+            alert("Provide a file please !")
+        } else {
+            document.getElementById("response").innerHTML = "Sending the file..."
+            document.getElementById("file_key_output").innerHTML = ""
+            // user datas
+            datas.append("chat_id", chatId)
+            datas.append("file", fileElement.files[0])
 
-	if (chatId === "" && chatId.length <= 5) {
-		alert("Provide a valid chat_id to save a file !")
-	} else {
-		if (fileElement.files.length === 0) {
-			alert("Provide a file please !")
-		} else {
-			document.getElementById("response").innerHTML = "Sending the file..."
-			// user datas
-			datas.append("chat_id", chatId)
-			datas.append("file", fileElement.files[0])
+            localStorage.setItem("chatId", chatId);
 
-			fetch(
-				"/api/upload",
-				{
-					method: "post",
-					body: datas,
-				}
-			)
-				.then((response) => {
-					return response.json()
-				})
-				.then((data) => {
-					if (data.status === 403) {
-						alert("Your file is too Large !")
-					} else if (data.status === "success") {
-						event.target.setAttribute("disabled", "false")
-						event.target.parentNode.reset()
-						new BSN.Modal("#hiw", {
-							content: `<div class="modal-body">
-                            <p class="alert alert-success">
-                            🎉 ${data.message}<p/>
-                            <p>
-                                <b>File name :</b> 
-                                <span class="text-primary">
-                                    ${data.json_map.file.file_name}
-                                    </span>
-                            </p>
-                            <label for="fileKey"><b>File key : </b></label>
-	                        <div class="input-group input-group-sm mb-3">
-	                            <input type="text" class="form-control"
-	                            value="${data.file_key}" id="fileKey">
-	                            <div class="input-group-append">
-	                            <button type="button" class="btn btn-secondary" onclick="copy()">Copy</button>
-	                            </div>
-	                        </div>
-                            <section>
-                            <b>Chunks :</b> ${data.json_map.cloud_map.length}
-                            </section>
-	                        <section><b>Timestamp :</b> ${new Date().toUTCString()}</section>
-                            <p class="alert alert-warning">Make sure to save that <b>File key</b>, because it's the only key that will allow you to regenerate your file !<p/>
-                                    </div>`,
-							backdrop: true,
-							keyboard: true,
-						}).show()
-						document.getElementById("response").style.display = "none"
-					} else if (data.status === "error") {
-						document.getElementById(
-							"response"
-						).innerHTML = `<p class="alert alert-danger">Something went wrong, please check your chat-id and your file</p>`
-					} else {
-						if (data.json_map.file_map.length <= 0) {
-							document.getElementById(
-								"response"
-							).innerHTML = `<p class="alert alert-danger">Something went wrong, please check your chat-id and your file</p>`
-						} else {
-							document.getElementById("response").innerHTML = responseHtml
-						}
-					}
-				})
-				.catch(function (error) {
-					console.log("Request failed - ", error)
-				})
-		}
-	}
+            fetch(
+                "/api/upload",{
+                    method: "post",
+                    body: datas,
+                })
+                .then((response) => {
+                    return response.json()
+                })
+                .then((data) => {
+                    if (data.status === 403) {
+                        alert("[+] Your file is too Large !")
+                    } else if (data.status === "success") {
+                        showSavedModal(data);
+                        document.getElementById("file_key_output").innerHTML = "File-key: " + data.file_key
+                        document.getElementById("response").style.display = "none"
+                    } else if (data.status === "error") {
+                        document.getElementById(
+                            "response"
+                        ).innerHTML = `<p class="alert alert-danger">Something went wrong, please check your chat-id and your file</p>`
+                    } else {
+                        if (data.json_map.file_map.length <= 0) {
+                            document.getElementById(
+                                "response"
+                            ).innerHTML = `<p class="alert alert-danger">Something went wrong, please check your chat-id and your file</p>`
+                        } else {
+                            document.getElementById("response").innerHTML = responseHtml
+                        }
+                    }
+                })
+                .catch(function (error) {
+                    console.log("Request failed - ", error)
+                });
+        }
+    }
 }
 
 const generateDownloadLink = () => {
