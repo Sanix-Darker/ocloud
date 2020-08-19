528c5e149340c57749c2718535f2d4af65802d23
sanix-darker <s4nixd@gmail.com>
2020-08-19 22:10:00 +0100

    Update on refreshing html and py files

--- app/server/cleaner.py
+++ app/server/cleaner.py
@@ -8,17 +8,13 @@ def main():
 	# initializing the count
 	deleted_folders_count = 0
 	deleted_files_count = 0
-
 	# specify the path
 	path = "./app/server/static/files/"
-
 	# specify the days
 	days = 1
-
 	# converting days to seconds
 	# time.time() returns current time in seconds
 	seconds = time.time() - (days * 24 * 60 * 60)
-
 	# checking whether the file is present in path or not
 	if os.path.exists(path):
 		# iterating over each and every folder and file in the path
@@ -40,7 +36,6 @@ def main():
 						# invoking the remove_folder function
 						remove_folder(folder_path)
 						deleted_folders_count += 1 # incrementing count
-
 				# checking the current directory files
 				for file in files:
 					# file path
@@ -50,7 +45,6 @@ def main():
 						# invoking the remove_file function
 						remove_file(file_path)
 						deleted_files_count += 1 # incrementing count
-
 		else:
 			# if the path is not a directory
 			# comparing with the days
@@ -58,7 +52,6 @@ def main():
 				# invoking the file
 				remove_file(path)
 				deleted_files_count += 1 # incrementing count
-
 	else:
 		# file/folder is not found
 		print(f'"{path}" is not found')
@@ -90,7 +83,6 @@ def remove_file(path):
 
 
 def get_file_or_folder_age(path):
-
 	# getting ctime of the file/folder
 	# time will be in seconds
 	ctime = os.stat(path).st_ctime
